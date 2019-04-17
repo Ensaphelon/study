3b8954da47d321ce446e06c828cba3ae48e9df76
Ramil Musaev <musaev.ramil@gmail.com>
2019-04-17 21:49:46 +0400

    Arrays exercise completed

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,33 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max = array.max
+        array.map { |number| number > 0 ? max : number }
       end
 
-      def search(_array, _query)
-        0
+      def search(_array, _query, index = 0)
+        length = _array.length
+
+        if length == 0
+          return -1
+        end
+
+        if length == 1
+          return _array[0] == _query ? index : -1
+        end
+
+        left_part, right_part = _array.each_slice((_array.size/2.0).round).to_a
+        current_value = left_part[-1]
+
+        if current_value == _query
+          return index
+        end
+
+        if current_value < _query
+          self.search(right_part, _query, left_part.length + index)
+        else
+          self.search(left_part, _query, index)
+        end
       end
     end
   end
