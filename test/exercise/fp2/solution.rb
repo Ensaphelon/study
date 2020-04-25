fe537f234d5f5eceac6b1c54eda2d1d9b385ca45
Ramil Musaev <musaev.ramil@gmail.com>
2020-04-26 02:31:59 +0400

    fp2 tasks competed

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,93 +1,37 @@
-require 'pry'
-
 module Exercise
   module Fp2
     class MyArray < Array
-      def remove_head(_array)
-        _array[1..(_array.length - 1)]
-      end
-
-      def first(_array)
-        _array[0]
-      end
-
-      def _each(_array, _initial_array, &block)
-        length = _array.length
-
-        if length > 0
-          current_element = first(_array)
-          rest_elements = remove_head(_array)
-
-          yield(current_element)
-
-          _each(rest_elements, _initial_array, &block)
-        end
-
-        MyArray.new(_initial_array)
-      end
-
-      def _map(_array, _modified_array, index, &block)
-        length = _array.length
-
-        if length > 0
-          current_element = first(_array)
-          rest_elements = remove_head(_array)
-          _modified_array[index] = yield(current_element)
-
-          _map(rest_elements, _modified_array, index + 1, &block)
-        end
-
-        MyArray.new(_modified_array)
-      end
-
-      def _compact(_array, _modified_array, &block)
-        length = _array.length
-
-        if length > 0
-          current_element = first(_array)
-          rest_elements = remove_head(_array)
-          index = _modified_array.length > 0 ? _modified_array.length : 0
-
-          if current_element != nil
-            _modified_array[index] = current_element
-          end
-
-          _compact(rest_elements, _modified_array, &block)
-        end
-
-        MyArray.new(_modified_array)
-      end
-
-      def _reduce(acc = nil, _array, &block)
-        accumulator_presents = acc != nil
-        length = _array.length
-
-        if length > 0
-          current_element = accumulator_presents ? first(_array) : _array[1]
-          rest_elements = remove_head(_array)
-
-          acc = accumulator_presents ? yield(acc, current_element) : first(_array)
-
-          _reduce(acc, rest_elements, &block)
-        else
-          acc
+      def my_each
+        for element in self
+          yield element
         end
       end
 
-      def my_each(&block)
-        _each(self, self, &block)
+      def my_map
+        new_array = MyArray.new
+        self.my_each do |element|
+          new_array.push(yield element)
+        end
+        new_array
       end
 
-      def my_map(&block)
-        _map(self, [], 0, &block)
+      def my_compact
+        new_array = MyArray.new
+        self.my_each do |element|
+          !element.nil? && new_array.push(element)
+        end
+        new_array
       end
 
-      def my_compact(&block)
-        _compact(self, [], &block)
-      end
-
-      def my_reduce(acc = nil, &block)
-        _reduce(acc, self, &block)
+      def my_reduce(initial = nil, &block)
+        has_initial = !initial.nil?
+        acc = has_initial ? initial : self[0]
+        first, *rest = self
+        array = has_initial ? self : MyArray.new(rest)
+        array.my_each do |element| 
+          acc = yield(acc, element) 
+        end
+        acc
       end
     end
   end
