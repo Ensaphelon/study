1b963b9ba0ae04998f78d892be1879333a15752c
Ramil Musaev <musaev.ramil@gmail.com>
2020-04-28 00:11:35 +0400

    refactored with recursion

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -1,37 +1,43 @@
 module Exercise
   module Fp2
     class MyArray < Array
-      def my_each
-        for element in self
-          yield element
+      def my_each(acc = MyArray.new([]), &func)
+        head, *tail = self
+        acc << head
+
+        yield head
+
+        if tail.empty?
+          acc
+        else
+          MyArray.new(tail).my_each(acc, &func)
         end
       end
 
       def my_map
-        new_array = MyArray.new
-        self.my_each do |element|
-          new_array.push(yield element)
+        my_reduce MyArray.new do |acc, element|
+          acc << yield(element)
         end
-        new_array
       end
 
       def my_compact
-        new_array = MyArray.new
-        self.my_each do |element|
-          !element.nil? && new_array.push(element)
+        my_reduce MyArray.new do |acc, element|
+          element.nil? ? acc : MyArray.new([*acc, element])
         end
-        new_array
       end
 
-      def my_reduce(initial = nil, &block)
+      def my_reduce(initial = nil, &func)
+        head, *tail = self
         has_initial = !initial.nil?
-        acc = has_initial ? initial : self[0]
-        first, *rest = self
-        array = has_initial ? self : MyArray.new(rest)
-        array.my_each do |element| 
-          acc = yield(acc, element) 
+        acc = has_initial ? initial : head
+        current_value = has_initial ? head : tail.first
+        new_acc = yield(acc, current_value)
+
+        if tail.empty?
+          new_acc
+        else
+          MyArray.new(tail).my_reduce(initial ? new_acc : head, &func)
         end
-        acc
       end
     end
   end
